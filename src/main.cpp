7ffd74214e761fb19447a0dd873ebd3bd4a83d01
edo9300 <edoardo762@gmail.com>
2019-08-20 17:22:34 +0200

    Added batch conversion support, updated deck naming structure
    Now deck will be named in this way: "replay_name playerX player_name.ydk"

--- src/main.cpp
+++ src/main.cpp
@@ -50,6 +50,19 @@ bool SaveDeck(const path_string &name, std::vector<int> mainlist,
     return true;
 }
 
+path_string GetFilename(path_string path) {
+	size_t dashpos = path.find_last_of(I18N("\\/"));
+	if(dashpos == path_string::npos)
+		dashpos = 0;
+	else
+		dashpos++;
+	size_t dotpos = path.find_last_of(I18N("."));
+	if(dotpos == path_string::npos)
+		dotpos = path.size();
+	path_string name = path.substr(dashpos, dotpos - dashpos);
+	return name;
+}
+
 #if defined(UNICODE)
 int wmain(int argc, wchar_t *argv[]) {
 #else
@@ -58,26 +71,28 @@ int main(int argc, char *argv[]) {
     if (argc < 2) {
 		return EXIT_FAILURE;
 	}
-    std::ifstream replay_file(argv[1], std::ifstream::binary);
-    if (!replay_file.is_open()) {
-		return EXIT_FAILURE;
+	for(int j = 1; j < argc; j++) {
+		std::ifstream replay_file(argv[j], std::ifstream::binary);
+		if(!replay_file.is_open()) {
+			return EXIT_FAILURE;
+		}
+		std::vector<uint8_t> contents((std::istreambuf_iterator<char>(replay_file)),
+									  std::istreambuf_iterator<char>());
+		replay_file.close();
+		Replay replay(contents);
+		auto players = replay.GetPlayerNames();
+		if(players.empty()) {
+			return EXIT_FAILURE;
+		}
+		auto decks = replay.GetPlayerDecks();
+		if(players.size() > decks.size()) {
+			return EXIT_FAILURE;
+		}
+		path_string filename = GetFilename(argv[j]);
+		for(int i = 0; i < decks.size(); i++) {
+			SaveDeck(filename + I18N("_") + I18N("player") + tostring(i) + I18N("_") + convert_string(players[i]) + I18N(".ydk"),
+					 decks[i].main_deck, decks[i].extra_deck);
+		}
 	}
-    std::vector<uint8_t> contents((std::istreambuf_iterator<char>(replay_file)),
-				  std::istreambuf_iterator<char>());
-    replay_file.close();
-    Replay replay(contents);
-    auto players = replay.GetPlayerNames();
-    if (players.empty()) {
-		return EXIT_FAILURE;
-	}
-    auto decks = replay.GetPlayerDecks();
-    if (players.size() > decks.size()) {
-		return EXIT_FAILURE;
-	}
-    replay.pheader.seed;
-    for (int i = 0; i < decks.size(); i++) {
-		SaveDeck(I18N("player ") + convert_string(players[i]) + I18N(" ") + tostring(i) + I18N(".ydk"),
-				 decks[i].main_deck, decks[i].extra_deck);
-    }
 	return EXIT_SUCCESS;
 }
