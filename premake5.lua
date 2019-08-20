2703123f2d6e642ee77b31d0d6e4a09b85e10a86
Edoardo <edoardo762@gmail.com>
2019-08-20 15:55:52 +0200

    Init

---
+++ premake5.lua
@@ -0,0 +1,53 @@
+workspace "net"
+location "build"
+language "C++"
+objdir "obj"
+staticruntime "on"
+startproject "netdecker"
+
+configurations { "Debug", "Release" }
+
+filter "system:windows"
+defines { "WIN32", "_WIN32" }
+
+filter "system:bsd"
+includedirs "/usr/local/include"
+libdirs "/usr/local/lib"
+
+filter "system:macosx"
+toolset "clang"
+buildoptions { "-fms-extensions" }
+includedirs { "/usr/local/include" }
+libdirs { "/usr/local/lib" }
+
+filter "action:vs*"
+vectorextensions "SSE2"
+defines "_CRT_SECURE_NO_WARNINGS"
+
+filter "action:not vs*"
+buildoptions { "-fno-strict-aliasing", "-Wno-multichar" }
+
+filter { "action:not vs*", "system:windows" }
+buildoptions { "-static-libgcc" }
+
+filter "configurations:Debug"
+symbols "On"
+defines "_DEBUG"
+targetdir "bin/debug"
+runtime "Debug"
+
+filter { "configurations:Release*" , "action:not vs*" }
+symbols "On"
+defines "NDEBUG"
+buildoptions "-march=native"
+
+filter "configurations:Release"
+optimize "Size"
+targetdir "bin/release"
+project "netdecker"
+kind "ConsoleApp"
+links { "clzma" }
+includedirs "lzma"
+files { "*.cpp", "*.cc", "*.c", "*.h" }
+
+include "lzma/."
